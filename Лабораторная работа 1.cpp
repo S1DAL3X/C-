ce08dae51fc70e1286465faef4e2e43e19856bc1
Aleksey <sidorenkoalekse@mail.ru>
2019-09-15 22:23:03 +0700

    Create Лабораторная работа 1.cpp

---
+++ Лабораторная работа 1.cpp
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+int main(){
+	double Y, x, a, b, c, s;
+	a = 3.5;
+	b = 4;
+	cout << "x = ";
+	cin >> x;
+	s = sin(pow(x, 2) + b);
+	c = cos(pow(x, 2) + b);
+
+	if (x > 0) {
+		Y = a * s;
+	}
+	else {
+		if (x == 0) {
+			Y = pow(x, 2) + b;
+		}
+		else {
+			Y = a * c;
+		}
+	}
+	cout << "Y = " << Y << endl;
+	return 0;
+}
