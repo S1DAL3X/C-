b5798ca406d0264df2f52b2badc4cff1a2a26237
Aleksey <sidorenkoalekse@mail.ru>
2019-09-22 12:44:01 +0700

    Update Лабораторная работа 2.cpp

--- Лабораторная работа 2.cpp
+++ Лабораторная работа 2.cpp
@@ -10,27 +10,32 @@
 using namespace std;
 
 int main(){
-	double x, c, t, Y, Min, Max, M_PI;
-	cout << setw(10) << "N0" << setw(10) << "t" << setw(10) << "x" << setw(10) << "Y" << endl;
-	M_PI = 3.1515;
-	Min =  10E10;
-	Max = -10E10;
+	double x, c, t, Y, MinY, MaxY, MinX, MaxX, M_PI;
+	cout << setw(10) << "N0" << setw(15) << "t" << setw(15) << "x" << setw(15) << "Y" << endl;
+	M_PI = 3.1515;			//Определили переменную M_PI вручную, как число Пи
+	MinY =  10E10;
+	MinX = 10E10;
+	MaxY = -10E10;
+	MaxX = -10E10;
 	t = M_PI / 2;
-	x = cos(t);
 	c = 1;
 	
 	for(t = M_PI / 2; t <= M_PI; t = t + (M_PI / 18)){
+		x = cos(t);
 		Y = cos(2*x) / 1 - sin(x) + pow(x, 3);
-		cout << setw(10) << c << setw(10) << t << setw(10) << x << setw(10) << Y << endl;
+		cout << setw(15) << c << setw(15) << t << setw(15) << x << setw(15) << Y << endl;
 		c+=1;
 
-		if(Y < Min){Min = Y;}	//Ищем минимальное Y
-		if(Y > Max){Max = Y;}   //Ищем максимальное Y
+		if(Y < MinY){MinY = Y;}	//Ищем минимальное Y
+		if(Y > MaxY){MaxY = Y;}   //Ищем максимальное Y
+
+		if(x < MinX){MinX = x;}	//Ищем минимальное X
+		if(x > MaxX){MaxX = x;}   //Ищем максимальное X
 	}
 
 	cout << endl;
-	cout << setw(10) << "Min Y" << setw(10) << "Max Y" << endl;
-	cout << setw(10) << Min		<< setw(10) << Max	   << endl;
+	cout << setw(15) << "Min Y" << setw(15) << "Max Y" << setw(15) << "Min X" << setw(15) << "Max X" << endl;
+	cout << setw(15) << MinY	<< setw(15) << MaxY	   << setw(15) << MinX << setw(15) << MaxX << endl;
 	getch();
 	return 0;
 }
