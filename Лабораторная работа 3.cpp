a897b9481dee1055960336f788877e99731061c7
Aleksey <sidorenkoalekse@mail.ru>
2019-10-16 19:04:01 +0700

    Update Лабораторная работа 3.cpp

--- Лабораторная работа 3.cpp
+++ Лабораторная работа 3.cpp
@@ -1,3 +1,4 @@
+// var 16
 // Вариант 16
 #include "stdafx.h"
 #include <iomanip>
@@ -6,7 +7,7 @@
 #include "conio.h"
 #include <string>
 
-int factorial(int q){							//Нахождение факториала числа
+int factorial(int q){									//Нахождение факториала числа
 	int i_fact = 1, res_fact = 1;
 	while(i_fact <= q){
 		res_fact = i_fact * res_fact;
@@ -16,24 +17,30 @@ int factorial(int q){							//Нахождение факториала числ
 }
 
 int main(){
-	double z, x, J, res, t = 1, i = 1;
-	int y = 1, k = 7;
-	std::cout << "Enter Z: " << std::endl;		//Вводим z = 0.1 / 0.5 / 1
-	std::cin >> z;
-	
-	x = pow(z, 2) / 15;
-	int k_pred = 1;								//k_pred - результат умножения всех чисел после факториала до их умножения на k
-	while(t > 0.001){
-		t = pow((pow(z, 2) / 2), i) / factorial(i) * k_pred * k;
+	double z, x, J, res, t = 1, i = 1, eps;
+	int y = 1, k = 7, k_pred = 1;
+
+	std::cout	<< "Enter EPS: " << std::endl;
+	std::cin	>> eps;
+	std::cout	<< "\nEnter Z: " << std::endl;
+	std::cin	>> z;
+
+	x = (z*z) / 15;
+	while(t > eps){
+		t = pow(-1, i) * pow((pow(z, 2) / 2), i) / (factorial(i) * k * k_pred); //k_pred - результат перемножения всех чисел после факториала
 		k = k_pred * k;
-		res = 1 - t;
+		res = 1 + t;
+
+		if (i == 1){}
+		else{
+			k_pred = k + 2;
+		}
+
 		i += 1;
-		k += 2;
 	}
 	J = x * res;
-	std::cout << J << std::endl; 
-	std::cout << t << std::endl; 
+	std::cout << "\nJ = " << J << std::endl;
+	std::cout << "t = " << t << std::endl;
 	getch();
 	return 0;
 }
-
