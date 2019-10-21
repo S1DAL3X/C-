1df215bc7ba3a20d8dcda6a60ba3c8c4a9fd9b69
Aleksey <sidorenkoalekse@mail.ru>
2019-10-21 22:12:51 +0700

    Update LABA4.cpp

--- LABA4.cpp
+++ LABA4.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 int main(){
 	double matrix[3][3] = {	{4.4, 0.6, 5.3},
-							{1.7, 5.5, 0},
-							{3.4, 1.8, 6.6}};
+				{1.7, 5.5, 0},
+				{3.4, 1.8, 6.6}};
 	//вывод начальной матрицы
 	cout << "First matrix A: " << endl;
 	for(int i = 0; i <= 2; i++){
